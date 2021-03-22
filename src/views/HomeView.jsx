ef51388244342e0a634dadb5faf1fe9e22dc2088
Ricardo Gambini <ricardo.gambini18@outlook.com>
2021-03-21 22:01:53 -0500

    fix docs

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {useHistory} from 'react-router-dom'
 // Components
 import Nav from '../components/Nav'
 import Header from '../components/Header'
@@ -8,6 +9,8 @@ import Contact from '../components/Contact'
 import Footer from '../components/Footer'
 
 const HomeView = () => {
+  const history = useHistory()
+
   return (
     <div className="HomeView">
       <Nav />
@@ -16,9 +19,11 @@ const HomeView = () => {
       <Modes />
       <Contact />
       <Footer />
-      <div className="HomeView__Back">
-        <p>API Docs</p>
-      </div>
+      <a href="https://buscaminas-backend.herokuapp.com/api/docs/">
+        <div className="HomeView__Back">
+          <p>API Docs</p>
+        </div>
+      </a>
     </div>
   )
 }
